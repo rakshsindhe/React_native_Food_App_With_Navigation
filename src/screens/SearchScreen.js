bdb94ed8d614229550a7af141a8a6703f587d1d3
rakshsindhe <35263835+rakshsindhe@users.noreply.github.com>
2020-12-13 22:34:09 +0530

    Added Yelp API request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBox from "../components/SearchBox";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 2,
+        term: searchQuery,
+        location: "san jose"
+      }
+    });
+
+    console.log(
+      "RESPONSE = ",
+      JSON.stringify(response.data.businesses, undefined, 2)
+    );
+
+    setRestaurants(response.data.businesses);
+  };
 
   return (
     <View style={styles.appWrapper}>
       <SearchBox
         query={searchQuery}
         handleOnSearch={q => setSearchQuery(q)}
-        handleOnSubmit={() => console.log("Submitted the Form")}
+        handleOnSubmit={searchApi}
       />
     </View>
   );
